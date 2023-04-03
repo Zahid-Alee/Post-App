84321d9f37748e8e6a5c8234ab5dfd0eac92fa4e
Zahid <zahid177617@gmail.com>
2023-04-03 11:20:57 +0500

    updated changes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const body_parser = require("body-parser");
 const cors = require("cors");
 const jwt = require('jsonwebtoken')
-const fetch = require('node-fetch'); 
+const Airtable = require('airtable');
+const base = new Airtable({ apiKey: 'key15JO6J5kG6vX6r' }).base('appYp6Kk4gEM1SMyc');
+
 const app = express();
 app.use(body_parser.urlencoded({ extended: true }));
 app.use(body_parser.json());
@@ -22,88 +24,112 @@ app.get("/api", (req, res) => {
   res.json(data);
 });
 
-app.post("/api/login", (req, res) => {
-  console.log(req.body);
-  const { email, password } = req.body;
-  if (email === 'hello' && password === 'hello') {
-        // create and sign JWT token
-        const token = jwt.sign({ email }, 'secretkey', { expiresIn: '1h' });
-        
-        // return the token to the client
-        res.json({ success: true, token });
-        console.log(token)
-      } else {
-        res.status(401).json({ success: false, message: 'Invalid credentials' });
-      }
+// app.post("/api/login", (req, res) => {
+//     console.log(req.body);
+//     const { email, password } = req.body;
+//     if (email === 'hello' && password === 'hello') {
+//           // create and sign JWT token
+//           const token = jwt.sign({ email }, 'secretkey', { expiresIn: '1h' });
+          
+//           // return the token to the client
+//           res.json({ success: true, token });
+//           console.log(token)
+//         } else {
+//           res.status(401).json({ success: false, message: 'Invalid credentials' });
+//         }
+//   });
+
+// const base = new Airtable({apiKey: 'YOUR_API_KEY'}).base('YOUR_BASE_ID');
+
+// Parse incoming requests with body parser
+// app.use(bodyParser.json());
+
+// Authenticate user and send JWT token
+app.post('/api/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Search for user in Airtable
+    base('Authentications')
+      .select({
+        filterByFormula: `AND({UserEmail} = "${email}", {UserPassword} = "${password}")`,
+      })
+      .firstPage((err, records) => {
+        if (err) throw err;
+
+        if (records.length > 0) {
+          // User exists, create and send JWT token
+          const token = jwt.sign({ email: email }, 'YOUR_SECRET_KEY', { expiresIn: '1h' });
+          res.json({ success: true, message: 'User authenticated!', token });
+        } else {
+          // User does not exist, send error message
+          res.status(401).json({ success: false, message: 'Invalid credentials!' });
+        }
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Internal server error!' });
+  }
 });
+  
 
 // handle protected API request
-app.post('/api/protected', (req, res) => {
-    // get the authorization header from the request
-    const authHeader = req.headers.authorization;
-    
-    if (authHeader) {
-      // extract the token from the header
-      const token = authHeader.split(' ')[1];
-      
-      try {
-        // verify the token
-        const decoded = jwt.verify(token, "secretkey");
-        
-        // if the token is valid, return protected data
-        const data = { message: 'Protected data received!', user: decoded.email };
-        res.json(data);
-      } catch (err) {
-        res.status(403).json({ success: false, message: 'Invalid token' });
-      }
-    } else {
-      res.status(401).json({ success: false, message: 'Missing authorization header' });
-    }
-});
-
 app.post("/api/signup", async (req, res) => {
-  try {
-    const { UserName, UserEmail, UserPassword } = req.body;
-    const tableName='Authentications';
-    const baseId='v0/appYp6Kk4gEM1SMyc';
-    const apiKey='key15JO6J5kG6vX6r';
-
-    // Airtable API endpoint URL for creating a new record
-    const url = `https://api.airtable.com/v0/${baseId}/${tableName}`;
-
-    // Airtable API request headers
-    const headers = {
-      Authorization: `Bearer ${apiKey}`,
-      "Content-Type": "application/json",
-    };
-
-    // Airtable API request body
-    const body = JSON.stringify({
-      fields: {
+    try {
+      const { UserName, UserEmail, UserPassword } = req.body;
+      const tableName = 'Authentications';
+  
+      // Airtable record data
+      const record = {
         "UserName": UserName,
         "UserEmail": UserEmail,
         "UserPassword": UserPassword,
-      },
-    });
+      };
+  
+      // Send the Airtable API request to create a new record
+      base(tableName).create(record, (err, record) => {
+        if (err) {
+          console.error(err);
+          res.json({ success: false, message: "Error occurred while creating the record!" });
+        } else {
+          console.log(record.getId());
+          res.json({ success: true, message: "Record created successfully!" });
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      res.json({ success: false, message: "Error occurred while creating the record!" });
+    }
+  });
 
-    // Send the Airtable API request
-    const response = await fetch(url, {
-      method: "POST",
-      headers,
-      body,
-    });
+app.post("/api/signup", async (req, res) => {
+    try {
+      const { UserName, UserEmail, UserPassword } = req.body;
+  
+      // Insert data into Airtable
+      base('Authentications').create([
+        {
+          "fields": {
+            "UserName": UserName,
+            "UserEmail": UserEmail,
+            "UserPassword": UserPassword
+          }
+        }
+      ], function(err, records) {
+        if (err) {
+          console.error(err);
+          res.json({ success: false, message: "Error occurred while creating the record!" });
+          return;
+        }
+        res.json({ success: true, message: "Record created successfully!" });
+      });
+  
+    } catch (error) {
+      console.error(error);
+      res.json({ success: false, message: "Error occurred while creating the record!" });
+    }
+  });
 
-    // Parse the response JSON
-    const data = await response.json();
-
-    // Send the response back to the client
-    res.json({ success: true, message: "Record created successfully!" });
-  } catch (error) {
-    console.error(error);
-    res.json({ success: false, message: "Error occurred while creating the record!" });
-  }
-});
-
-app.listen(5000, () => {
+app.listen(5001, () => {
   console.log("Server is running on port 5001");
 });
